d469c24a674d2f87415aace5cbbf8e326d98aee0
goutamiydamodar7-lab <goutamiydamodar7@gmail.com>
2026-03-13 14:56:42 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/auth.routes.js";
 import postRoutes from "./routes/posts.routes.js";
 import searchRoutes from "./routes/search.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import activityRoutes from "./routes/activity.routes.js";
 
 import likesFollowRoutes from "./routes/likes-follow.routes.js";
 import { errorHandler } from "./middleware/error.js";
@@ -44,6 +45,8 @@ app.use("/api/v1", likesFollowRoutes);
 app.use("/api/v1/search", searchRoutes);
 
 app.use("/api/v1/users", userRoutes);
+
+app.use("/api/v1/activity", activityRoutes);
 
 app.use(errorHandler); // must be last
 
---
+++ src/routes/activity.routes.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { getActivity } from "../controllers/activity.controller.js";
+import { auth } from "../middleware/auth.js";
+
+const router = express.Router();
+router.get("/", auth, getActivity);
+export default router;
