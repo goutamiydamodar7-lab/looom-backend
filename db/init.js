23fbf0acdbcaa13b85f49898d3ee2a7ee7e998fd
goutamiydamodar7-lab <goutamiydamodar7@gmail.com>
2026-03-20 14:37:35 +0530

    ok

--- db/init.js
+++ db/init.js
@@ -1,20 +1,19 @@
-// internal library of node.js//
 import fs from "fs";
 import path from "path";
-
 import { pool } from "../src/db.js";
 
 export async function initDatabase() {
-    try{
-        const schemapath = path.resolve('db/schema.sql');
-        const sql = fs.readFileSync(schemapath,"utf-8");
-        await pool.query(sql);
-        console.log("Database schema initialized");
+  try {
+    const schemaPath = path.join(process.cwd(), "db", "schema.sql");
 
-    }catch(err){
-        console.error("Database Initialization failed:", err);
+    console.log("Schema Path:", schemaPath); // debug
 
-    }
+    const sql = fs.readFileSync(schemaPath, "utf-8");
+
+    await pool.query(sql);
+
+    console.log("Database schema initialized");
+  } catch (err) {
+    console.error("Database Initialization failed:", err);
+  }
 }
-
-
